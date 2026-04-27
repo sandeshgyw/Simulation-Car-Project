ec99a26f625ab4c02e5c0062cf59379b3901ae9e
AlifurkanKaraman <alifurkankaraman061@gmail.com>
2026-04-26 20:43:39 -0500

    feat: implement Car entity initialization with physics and sensor state

--- simulation.py
+++ simulation.py
@@ -1,3 +1,10 @@
+import math
+import random
+import sys
+import os
+
+import neat
+import pygame
 
 WIDTH = 1600
 HEIGHT = 880
@@ -8,3 +15,26 @@ CAR_SIZE_Y = 30
 BORDER_COLOR = (255, 255, 255, 255) 
 
 current_generation = 0 
+
+class Car:
+
+    def __init__(self):
+        self.sprite = pygame.image.load('car.png').convert() # Convert Speeds Up A Lot
+        self.sprite = pygame.transform.scale(self.sprite, (CAR_SIZE_X, CAR_SIZE_Y))
+        self.rotated_sprite = self.sprite 
+
+        self.position = [690, 740] 
+        self.angle = 0
+        self.speed = 0
+
+        self.speed_set = False 
+
+        self.center = [self.position[0] + CAR_SIZE_X / 2, self.position[1] + CAR_SIZE_Y / 2] # Calculate Center
+
+        self.radars = [] 
+        self.drawing_radars = [] 
+
+        self.alive = True
+
+        self.distance = 0 
+        self.time = 0 
